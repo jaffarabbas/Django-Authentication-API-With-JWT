90b21eac96be5084692adcd74fab1bd6c8b9ef51
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-07 17:56:56 +0500

    Updated account/views.py at 07/07/2022 17:56:56

--- DRF_AuthAPI/account/views.py
+++ DRF_AuthAPI/account/views.py
@@ -1,7 +1,13 @@
+from turtle import st
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from account.serializers import UserRegistrationSerializer
 
 class UserRegistration(APIView):
-    def get(self, request, format=None):
-        return Response({'msg':'Registration successful'})
+    def post(self, request, format=None):
+        serializers = UserRegistrationSerializer(data=request.data)
+        if serializers.is_valid(raise_exception=True):
+            user = serializers.save()
+            return Response({'msg':'Registration successful'},status = status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
