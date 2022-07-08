27bba0f1807b216d329d1bd81bd4dc830c44e6b2
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-08 18:36:07 +0500

    Updated account/views.py at 08/07/2022 18:36:07

--- DRF_AuthAPI/account/views.py
+++ DRF_AuthAPI/account/views.py
@@ -2,20 +2,33 @@ from turtle import st
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from account.serializers import UserRegistrationSerializer,UserLoginSerializer
+from account.serializers import UserRegistrationSerializer,UserLoginSerializer,UserProfileSerializer
 from django.contrib.auth import authenticate
 from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
 
-class UserRegistration(APIView):
+#generate token manually 
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+class UserRegistrationView(APIView):
     renderer_classes = [UserRenderer]
     def post(self, request, format=None):
         serializers = UserRegistrationSerializer(data=request.data)
         if serializers.is_valid(raise_exception=True):
             user = serializers.save()
-            return Response({'msg':'Registration successful'},status = status.HTTP_201_CREATED)
+            token = get_tokens_for_user(user)
+            return Response({'token':token,'msg':'Registration successful'},status = status.HTTP_201_CREATED)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class UserLogin(APIView):
+class UserLoginView(APIView):
+    renderer_classes = [UserRenderer]
     def post(self, request, format=None):
         serializers = UserLoginSerializer(data=request.data)
         if serializers.is_valid(raise_exception=True):
@@ -23,6 +36,14 @@ class UserLogin(APIView):
             password = serializers.data.get('password')
             user = authenticate(email=email, password=password)
             if user is not None:
-                return Response({'msg':'Login successful'},status = status.HTTP_200_OK)
+                token = get_tokens_for_user(user)
+                return Response({'token':token,'msg':'Login successful'},status = status.HTTP_200_OK)
             else:
                 return Response({'errors':{'non_field_errors':['Email or Password is not Valid']}}, status=status.HTTP_400_BAD_REQUEST) 
+
+class UserProfileView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
+    def get(self, request, format=None):
+        serializers = UserProfileSerializer(request.user)
+        return Response(serializers.data,status = status.HTTP_200_OK)
