70e47405e6c59e072085c81572bf546ec650a0d6
Jaffar Abbas <gamaportal8@gmail.com>
2022-07-09 13:56:35 +0500

    Updated account/urls.py at 09/07/2022 13:56:35

--- DRF_AuthAPI/account/urls.py
+++ DRF_AuthAPI/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path , include
-from account.views import UserRegistrationView ,UserLoginView ,UserProfileView
+from account.views import UserRegistrationView ,UserLoginView ,UserProfileView , UserChangePassowrdView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(),name='register'),
     path('login/', UserLoginView.as_view(),name='login'),
-    path('profile/', UserProfileView.as_view(),name='profile')
+    path('profile/', UserProfileView.as_view(),name='profile'),
+    path('changepassowrd/', UserChangePassowrdView.as_view(),name='changepassowrd')
 ]
